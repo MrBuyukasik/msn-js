ea0b05c16f70de36cb738b8f97a345b7572ce2eb
Hasan Can <hasancanbuyukasik@gmail.com>
2022-02-13 00:10:52 +0300

    changed config connection

--- frontend/src/configs/config_connections.js
+++ frontend/src/configs/config_connections.js
@@ -1,6 +1,6 @@
 const config = () => {
     return {
-        ipServer: "msn-js.herokuapp.com",
+        ipServer: "mrbuyukasik-msnjs.herokuapp.com",
         // ipServer: "msn-js.herokuapp.com",
         // ipServer: "http://localhost:80",
     };
