0c075ae4e8c36a697de64e9117a9cbd693c69d7e
Hasan Can <hasancanbuyukasik@gmail.com>
2022-02-13 00:37:42 +0300

    reverted config

--- backend/configs/configs.js
+++ backend/configs/configs.js
@@ -7,6 +7,6 @@ module.exports = () => {
         ipServer: ipServer,
         client_id: "1e1f069dfd634fb6ac68e462b7e10e92", // Your client id
         client_secret: "875dec3ee1ec426696d5a6e724c7cb99", // Your secret
-        redirect_uri: `https://${ipServer}/routes/spotify/callback`, // Your redirect uri
+        redirect_uri: `http://${ipServer}/routes/spotify/callback`, // Your redirect uri
     };
 };
