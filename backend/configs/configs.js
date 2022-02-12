4ff2b16f9bf9339b7a1646058e32d8208589ae09
Hasan Can <hasancanbuyukasik@gmail.com>
2022-02-12 23:54:31 +0300

    client_id updated

--- backend/configs/configs.js
+++ backend/configs/configs.js
@@ -5,8 +5,8 @@ module.exports = () => {
 
     return {
         ipServer: ipServer,
-        client_id: "3e7faf2a2ea6459586e73226950e4644", // Your client id
-        client_secret: "24f3a2a592e64fa29976cec753bf789f", // Your secret
+        client_id: "1e1f069dfd634fb6ac68e462b7e10e92", // Your client id
+        client_secret: "875dec3ee1ec426696d5a6e724c7cb99", // Your secret
         redirect_uri: `http://${ipServer}/routes/spotify/callback`, // Your redirect uri
     };
 };
